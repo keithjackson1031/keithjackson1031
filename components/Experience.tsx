0aae175809519d2f6448a731ef11d05546a58340
keithjackson1031 <keithjackson911031@gmail.com>
2020-05-07 08:09:00 +0300

    completed experience part

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -16,6 +16,7 @@ const Experience = () => {
         'Cooperated with PM and other teammates to enhance service and products.',
       ],
       technologies: ['React.js', 'Material UI', 'JavaScript'],
+      url: 'https://www.hyly.ai'
     },
     {
       company: 'Space ID',
@@ -28,6 +29,7 @@ const Experience = () => {
         'Developed smart contract code and test it in testnet and publish it to mainnet.',
       ],
       technologies: ['Web3', 'Ethers.js', 'Smart Contract', 'BNB Network'],
+      url: 'https://www.space.id/'
     },
     {
       company: 'Hi',
@@ -39,32 +41,43 @@ const Experience = () => {
         'Fix issues and maintained staking website.',
       ],
       technologies: ['Next.js', 'Web3', 'React'],
+      url: 'https://www.hi.com'
     },
   ];
 
   return (
     <section
       id="experience"
-      className="min-h-screen flex items-start px-8 lg:pl-[340px] pt-20 pb-20"
+      className="mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24 px-8 lg:pl-[100px] pt-1 pb-20"
     >
       <div className="max-w-[700px] w-full">
         <div className="space-y-12">
           {experiences.map((exp, index) => (
             <div key={index} className="relative">
-              <div className="flex items-start gap-6">
-                <div className="flex-shrink-0 w-24">
-                  <span className="text-xs text-[#8892b0] font-mono uppercase">
-                    {index + 1}. {exp.period}
+              <div className='group relative grid pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50'>
+                <header className="z-10 mb-2 font-semibold uppercase tracking-wide text-slate-500 sm:col-span-2">
+                  <span className="text-sm text-[#8892b0] font-mono uppercase">
+                    {exp.period}
                   </span>
-                </div>
-                <div className="flex-1">
-                  <h3 className="text-lg font-semibold text-[#ccd6f6] mb-1">
-                    {exp.position}
-                    <span className="text-[#64ffda]"> · {exp.company}</span>
+                </header>
+                <div className="z-10 sm:col-span-6">
+                  <h3 className="font-medium leading-snug text-slate-200">
+                    <div>
+                      <a className="inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-teal-300 focus-visible:text-teal-300  group/link text-xl" href={`${exp.url}`} target="_blank" rel="noreferrer noopener" aria-label="Senior Frontend Engineer, Accessibility at Klaviyo (opens in a new tab)">
+                      <span className="absolute -inset-x-4 -inset-y-2.5 hidden rounded md:-inset-x-6 md:-inset-y-4 lg:block"></span>
+                      <span>{exp.position} · <span className="inline-block">{exp.company}
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="inline-block h-4 w-4 shrink-0 transition-transform group-hover/link:-translate-y-1 group-hover/link:translate-x-1 group-focus-visible/link:-translate-y-1 group-focus-visible/link:translate-x-1 motion-reduce:transition-none ml-1 translate-y-px" aria-hidden="true">
+                          <path fill-rule="evenodd" d="M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z" clip-rule="evenodd">
+                          </path>
+                        </svg>
+                      </span>
+                      </span>
+                      </a>
+                    </div>
                   </h3>
-                  <ul className="mt-3 mb-4 space-y-2">
+                  <ul className="mt-3 mb-4 space-y-2 list-none">
                     {exp.responsibilities.map((resp, respIndex) => (
-                      <li key={respIndex} className="text-[#8892b0] text-[15px] leading-[1.7]">
+                      <li key={respIndex} className="text-[#8892b0] text-[17px] leading-[1.7]">
                         {resp}
                       </li>
                     ))}
@@ -73,7 +86,7 @@ const Experience = () => {
                     {exp.technologies.map((tech, techIndex) => (
                       <span
                         key={techIndex}
-                        className="px-2.5 py-1 text-xs font-mono text-[#8892b0] border border-[#233554] rounded"
+                        className="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300"
                       >
                         {tech}
                       </span>
